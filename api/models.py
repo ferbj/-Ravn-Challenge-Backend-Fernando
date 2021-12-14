ffccb2e2b9305c97cd277e10d1539cf58889917d
ferbj <ferbj200@gmail.com>
2021-12-14 01:12:51 -0500

    adding caching to some models

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.utils.functional import cached_property
+# Create your models here. 
 
-# Create your models here.
 class Author(models.Model):
     id=models.AutoField(primary_key=True)
     name=models.TextField()
@@ -8,11 +9,11 @@ class Author(models.Model):
     
     class Meta:
         db_table = "authors"
-
+    @cached_property 
     def AuthorName(self):
         str = "Name: {0}"
         return str.format(self.name,self.date_of_birth)
-    
+    @cached_property 
     def __str__(self):
         return self.AuthorName()
 
@@ -23,11 +24,11 @@ class Book(models.Model):
     
     class Meta:
         db_table = "books"
-    
+    @cached_property 
     def Bookname(self):
         str_author = "Author: {0}, Isbn: {1}"
         return str_author.format(self.author,self.isbn)
-    
+    @cached_property 
     def __str__(self):
         return self.Bookname()
 
@@ -40,10 +41,10 @@ class SaleItem(models.Model):
 
     class Meta:
         db_table = "sale_items"
-
+    @cached_property 
     def Sales(self):
         str = "Custommer: {0}, Price:{1}, Quantity:{2}"
         return str.format(self.customer_name,self.item_price,self.quantity)
-    
+    @cached_property 
     def __str__(self):
         return self.Sales()
--- api/views.py
+++ api/views.py
@@ -3,12 +3,10 @@ from rest_framework import  viewsets
 # Create your views here.
 from django.shortcuts import render
 from django.db import connection
-from django.core.cache import cache
-class AuthorView(viewsets.ModelViewSet):
 
+class AuthorView(viewsets.ModelViewSet):
     
     def count(request,id=None):
-        #authors = cache.get('authors')
         if(request.method=='GET' and id!=None):
             sql= 'SELECT au.id,au.name as author,SUM(sale_items.item_price) as sales_total FROM authors as au ' ' inner join books on books.author_id=au.id ' ' inner join sale_items on books.id=sale_items.book_id ' ' GROUP BY au.id,au.name ' ' ORDER BY sales_total DESC ' ' limit '+str(id)
             with connection.cursor() as cursor:
@@ -16,7 +14,6 @@ class AuthorView(viewsets.ModelViewSet):
                     cursor.execute(sql)
                     columns = [col[0] for col in cursor.description]        
                     authors = [dict(zip(columns, row)) for row in cursor.fetchall()]
-                    #cache.set('authors',authors,5)
                     return JsonResponse(authors,safe=False)
                 except Exception as e:
                     print(e)
